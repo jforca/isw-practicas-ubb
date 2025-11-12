7ab60217219d8b737ab25d5aa715f19db7011cab
Kevin Cardenas <kevcardenas675@gmail.com>
2025-11-11 21:37:08 -0300

    feat: Crea e integra tabla logbook-entries

--- apps/backend/src/entities/index.ts
+++ apps/backend/src/entities/index.ts
@@ -2,3 +2,4 @@ export { User } from './user.entity';
 export { Account } from './account.entity';
 export { Session } from './session.entity';
 export { Verification } from './verification.entity';
+export { LogbookEntries } from './LogbookEntries.entity';
---
+++ apps/backend/src/entities/logbook-entries.entity.ts
@@ -0,0 +1,33 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class LogbookEntries {
+	@PrimaryColumn({ type: 'int' })
+	id: number; //better auth
+
+	@Column({ type: 'varchar', length: 155 })
+	title: string;
+
+	@Column({
+		type: 'timestamp',
+		default: () => 'CURRENT_TIMESTAMP',
+	})
+	created_at: Date;
+
+	@Column({
+		type: 'timestamp',
+		default: () => 'CURRENT_TIMESTAMP',
+		onUpdate: 'CURRENT_TIMESTAMP',
+	})
+	updated_at: Date;
+
+	/*
+    @ManyToOne(() => Interships, intership => intership.reports)
+    @JoinColumn({ name: 'internship_id' })
+    internship: Interships;
+    */
+	/*
+    @Column({ type: 'int' })
+    internship_id: number; 
+    */
+}
