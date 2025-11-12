190da0bc883f5adf725ebb8fe39e29ef51588ef7
jforca <forcaelj@gmail.com>
2025-11-12 14:09:09 -0300

    feat: agregar rutas y controladores

--- apps/backend/src/routes/index.routes.ts
+++ apps/backend/src/routes/index.routes.ts
@@ -1,11 +1,13 @@
 import { Express, Router } from 'express';
 import InternshipCentersRouter from '@routes/internship-centers.route';
 import { studentsRouter } from './students.routes';
+import OffersRouter from '@routes/offers.route';
 
 export function routerApi(app: Express) {
 	const api = Router();
 	api.use('/internship-centers', InternshipCentersRouter);
 	api.use('/students', studentsRouter);
+	api.use('/offers', OffersRouter);
 
 	// Empezar a usar las rutas bajo /api
 	app.use('/api', api);
