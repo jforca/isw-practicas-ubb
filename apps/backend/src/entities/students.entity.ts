9385435b897706546d989a94a1bf7ef326adfb7c
jforca <forcaelj@gmail.com>
2025-11-12 00:13:00 -0300

    feat: Agregar id string

--- apps/backend/src/entities/students.entity.ts
+++ apps/backend/src/entities/students.entity.ts
@@ -9,6 +9,7 @@ import { User } from './user.entity';
 @Entity()
 export class Student {
 	@PrimaryColumn({ type: 'text' })
+	id: string;
 
 	@OneToOne(() => User, { onDelete: 'CASCADE' })
 	@JoinColumn({ name: 'id' })
